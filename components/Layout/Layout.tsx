a2e3d5aea64a9fbcf36c5630df2608306659902a
jackyef <jacky.efendi1@gmail.com>
2022-01-27 19:59:36 +0700

    Add page layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,14 @@
 import { Grid } from '@geist-ui/core'
-import type { NextPage } from 'next'
+import type { FC } from 'react'
 
-const Home: NextPage = () => {
+export const Layout: FC = ({ children }) => {
   return (
     <Grid.Container gap={2}>
       <Grid xs={0} sm={4} />
       <Grid xs={24} sm={16}>
-        Hello world
+        {children}
       </Grid>
       <Grid xs={0} sm={4} />
     </Grid.Container>
   )
 }
-
-export default Home
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
+import { Layout } from '@/components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GeistProvider>
       <CssBaseline />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </GeistProvider>
   )
 }

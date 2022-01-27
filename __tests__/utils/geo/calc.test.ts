d87d1079f1348b430be3fe910598023b1da1912a
jackyef <jacky.efendi1@gmail.com>
2022-01-27 20:37:22 +0700

    Add getDistance and test

---
+++ __tests__/utils/geo/calc.test.ts
@@ -0,0 +1,21 @@
+import { getBearing, getDistance } from '@/utils/geo/calc'
+
+describe('utils/geo/calc', () => {
+  describe('getBearing', () => {
+    it('returns bearing in degrees given a lat/lng pairs', () => {
+      const pointA = { lat: 39.099912, lng: -94.581213 }
+      const pointB = { lat: 38.627089, lng: -90.200203 }
+      const bearing = getBearing(pointA, pointB)
+      expect(bearing).toBe(96.51262423499946)
+    })
+  })
+
+  describe('getDistance', () => {
+    it('returns distance in km given a lat/lng pairs', () => {
+      const pointA = { lat: 39.099912, lng: -94.581213 }
+      const pointB = { lat: 38.627089, lng: -90.200203 }
+      const bearing = getDistance(pointA, pointB)
+      expect(bearing).toBe(382.90005037560167)
+    })
+  })
+})
--- utils/geo/calc.ts
+++ utils/geo/calc.ts
@@ -28,3 +28,25 @@ export const getBearing = (pointA: Point, pointB: Point) => {
 
   return (bearing + 360) % 360
 }
+
+export const getDistance = (pointA: Point, pointB: Point) => {
+  const { lat: lat1, lng: lng1 } = pointA
+  const { lat: lat2, lng: lng2 } = pointB
+
+  const earthRadius = 6371 // km
+
+  const dLat = toRadians(lat2 - lat1)
+  const dLng = toRadians(lng2 - lng1)
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) *
+      Math.cos(toRadians(lat2)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2)
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  const d = earthRadius * c
+
+  return d
+}

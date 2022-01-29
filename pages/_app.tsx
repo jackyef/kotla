487b5c75e9aa11076435f2dbd848e0bc1699809a
jackyef <jacky.efendi1@gmail.com>
2022-01-29 22:44:49 +0700

    Only track on prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,23 +11,27 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <KotlaProvider>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-0RT1WW0W2K"
-      ></Script>
-      <Head>
-        {/* Global site tag (gtag.js) - Google Analytics */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-0RT1WW0W2K"
+          ></Script>
+          <Head>
+            {/* Global site tag (gtag.js) - Google Analytics */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
               gtag('config', 'G-0RT1WW0W2K');`
-          }}
-        ></script>
-      </Head>
+              }}
+            ></script>
+          </Head>
+        </>
+      )}
       <Toaster gutter={32} toastOptions={toastOptions} />
       <Layout>
         <Component {...pageProps} />

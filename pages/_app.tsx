850c2522faede228a71ac3bfff84fb8068907ddd
jackyef <jacky.efendi1@gmail.com>
2022-01-29 22:42:13 +0700

    Add GA. Just curious about page views

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,29 @@ import { KotlaProvider } from '@/contexts/Kotla'
 import { Toaster } from 'react-hot-toast'
 
 import { toastOptions } from '@/lib/toast'
+import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <KotlaProvider>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-0RT1WW0W2K"
+      ></Script>
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-0RT1WW0W2K');`
+          }}
+        ></script>
+      </Head>
       <Toaster gutter={32} toastOptions={toastOptions} />
       <Layout>
         <Component {...pageProps} />
